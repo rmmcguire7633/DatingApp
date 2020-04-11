96317ff14c132ab2d2714896867dc265ebe8ebb7
rmmcguire7633 <35510316+rmmcguire7633@users.noreply.github.com>
2020-04-10 23:33:51 -0400

    Added some style and alerts

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,14 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {console.log('register success')},
-      error => {console.log(error); }
+    this.authService.register(this.model).subscribe(() => {this.alertify.success('register success')},
+      error => {this.alertify.error(error); }
       );
   }
 
